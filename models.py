f17541c9ab3612f01dd501d199632751fc75a51d
Kirill <verikirill@mail.ru>
2025-04-01 11:06:08 +0300

    upd

--- models.py
+++ models.py
@@ -3,10 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
-class Megabait(Base):
-    __tablename__ = "megabait"
-
+class User(Base):
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, nullable=True, index=True)
     password = Column(String, nullable=True)
