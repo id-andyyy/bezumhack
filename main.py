7291be97242c5e45ff5636afbf361a2738074a49
Andy <obrezkov.andy@gmail.com>
2025-04-01 10:44:54 +0300

    табличечка

--- main.py
+++ main.py
@@ -70,14 +70,14 @@ def register(
     password: str = Form(...),
     db: Session = Depends(get_db)
 ):
-    user_exists = db.query(models.User).filter(models.User.username == username).first()
+    user_exists = db.query(models.Megabait).filter(models.User.username == username).first()
     if user_exists:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Пользователь с таким именем уже существует"
         )
     
-    new_user = models.User(username=username, password=password)
+    new_user = models.Megabait(username=username, password=password)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
